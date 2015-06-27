87d7d4695cbeb0816c12854b9ed7f17cbeb12aed
Marc Christensen <mchristensen@control4.com>
2015-06-27 09:13:17 -0600

    Migrate GPIO manipulation code into a library, General cleanup

---
+++ src/rcsensor.c
@@ -0,0 +1,48 @@
+/*
+Calculate R/C charge time of capacitive sensors on Raspberry Pi GPIOs
+Python module
+ */
+#include <getopt.h>
+#include <Python.h>
+#include "rc_count.h"
+#define DEFAULT_DISCHARGE_DELAY 10
+#define DEFAULT_TIMEOUT 1000
+#define DEFAULT_CYCLES 10
+
+static PyObject* measure(PyObject *self, PyObject *args) {
+
+    unsigned int ret;
+    int gpio = -1;
+    int cycles = -1;
+    int discharge_delay = -1;
+
+    if (!PyArg_ParseTuple(args, "iii", &gpio, &cycles, &discharge_delay))
+        return NULL;
+
+    // Required arguments
+    if (gpio == -1){
+        exit(-1);
+    }
+    // default values
+    if (discharge_delay == -1)
+        discharge_delay = DEFAULT_DISCHARGE_DELAY;
+    if (cycles == -1)
+        cycles = DEFAULT_CYCLES;
+
+    ret = get_count(gpio, cycles, discharge_delay);
+    return Py_BuildValue("i", ret);
+}
+
+static char rcsensor_docs[] =
+    "measure(): return a value which correlates to the charge time of an RC sensor.\n";
+
+static PyMethodDef rcsensor_funcs[] = {
+    {"measure", (PyCFunction)measure, METH_VARARGS,
+    rcsensor_docs},
+    {NULL, NULL, 0, NULL}
+};
+
+void initrcsensor(void)
+{
+    Py_InitModule("rcsensor", rcsensor_funcs);
+}
--- src/rcsensor_cli.c
+++ src/rcsensor_cli.c
@@ -6,20 +6,20 @@ Calculate R/C charge time of capacitive sensors on Raspberry Pi GPIOs
 #include <string.h>
 #include <errno.h>
 #include <stdlib.h>
-#include <wiringPi.h>
 #include <getopt.h>
+#include "rc_count.h"
 #define DEFAULT_DISCHARGE_DELAY 10
 #define DEFAULT_TIMEOUT 1000
 #define DEFAULT_CYCLES 10
 
 void usage(void) {
-    fprintf (stderr, "Usage: moisture_sense -g <RPi GPIO> -c [cycles (ms): 1000] -d [capacitor discharge time (ms): 10] -t [timeout (ms): 1000].\n");
+    fprintf (stderr, "Usage: moisture_sense -g <RPi GPIO> -c [cycles (ms): 1000] -d [capacitor discharge time (ms): 10].\n");
 }
 
 // main
 int main(int argc, char *argv[]) {
 
-    unsigned int c,x,re[1000],med=0;
+    unsigned int c,x,med=0,val;
     int cycles = -1;
     int discharge_delay = -1;
     int gpio = -1;
@@ -29,11 +29,6 @@ int main(int argc, char *argv[]) {
         switch (option) {
           case 'c':
             cycles = atof(optarg);
-            if (cycles>1000) {
-                fprintf(stderr, "Cycles must be less than 1000.\n");
-                usage();
-                exit(-1);
-            }
             break;
           case 'd':
             discharge_delay = atof(optarg);
@@ -62,31 +57,9 @@ int main(int argc, char *argv[]) {
         discharge_delay = DEFAULT_DISCHARGE_DELAY;
     if (cycles == -1)
         cycles = DEFAULT_CYCLES;
+    unsigned int re[cycles];
 
-    wiringPiSetup();
-    int pin;
-    for (pin = 0 ; pin < 16 ; ++pin) {
-        pinMode (pin, OUTPUT) ;
-        digitalWrite (pin, LOW) ;
-    }
-
-    if (wiringPiSetupGpio()<0)
-        exit (1) ;
-
-    for (x=0;x<cycles;x++) {
-        c=0;;
-        pinMode (gpio, OUTPUT);;
-        digitalWrite (gpio, LOW);
-        delay(discharge_delay);
-        pinMode (gpio, INPUT);
-        while (digitalRead(gpio)==LOW) {
-            c++;
-        }
-        re[x]=c;
-    }
-    med=0;
-    for (x=0;x<cycles;x++)
-        med+=re[x];
-    printf("%d\n",med/cycles);
+    val = get_count(gpio, cycles, discharge_delay);
+    printf("%d\n",val);
     return 0;
 }
