3c7e6e1f02b9d73497cb49359d542d3fa4c9a85f
Marc Christensen <mchristensen@control4.com>
2015-07-02 19:48:04 -0600

    Create a RC sensor class object

--- utils/rc_sensor.py
+++ utils/rc_sensor.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python
 
-import rcsensor
+from common.rcsensor import rcsensor as rcsensor
 
-print(rcsensor.get_count(200, 10, 22))
+
+class RcSensor(object):
+
+    def __init__(self, gpio, cycles=200, discharge_delay=10):
+        if gpio is None:
+            raise ValueError("Must supply gpio value")
+        self.gpio = gpio
+        self.cycles = cycles
+        self.discharge_delay = discharge_delay
+
+    def rc_count(self):
+        """
+        Returns the average of cycle number of readings from a GPIO based R/C sensor
+
+        :return: int
+        """
+        return rcsensor.get_rc_counts(self.gpio, self.cycles, self.discharge_delay)
