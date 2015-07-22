29dd5e4fc295cc0fcd528a673257758f5029d9cd
Marc Christensen <mchristensen@control4.com>
2015-07-21 20:35:25 -0600

    Clean up code related to old GPIO interface
    Start Vegetronix analog moisture sensor support

---
+++ common/moisture_sensor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from common.adafruit.Adafruit_ADS1x15 import Adafruit_ADS1x15
+
+class MoistureSensor(object):
+
+    def __init__(self):
+        pass
+
+    def read_percent(self):
+        pass
+
+    def read_raw(self):
+        pass
