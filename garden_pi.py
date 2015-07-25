a65b92bbe4d0ab003080757d9689e694a80df9df
Marc Christensen <mchristensen@control4.com>
2015-07-25 14:52:34 -0600

    Remove support for ambient light sensor for now Change gpio support to MCP GPIO chip support Create a new watering function for scheduled waterings Change existing watering funcion to water on moisture sensor values

--- garden_pi.py
+++ garden_pi.py
@@ -6,7 +6,6 @@ GPIO.setwarnings(False)
 from common.zone import Zone
 from common.cpu_temp import CpuTemp
 from common.temp_sensor import TempSensor
-from common.rc_sensor_cli import RcSensor
 from common.utils import run_as_thread
 from common.utils import format_float
 import common.csv_logger as cvs_logger
@@ -28,7 +27,7 @@ DEBUG = conf_parser.getboolean('main', 'debug')
 ambient_temp_sensor_id = conf_parser.get('main', 'ambient_temp_sensor')
 ambient_temp_sensor = TempSensor(ambient_temp_sensor_id)
 ambient_light_sensor_gpio = conf_parser.getint('main', 'ambient_light_sensor_gpio')
-ambient_light_sensor = RcSensor(gpio=ambient_light_sensor_gpio)
+ambient_light_sensor = None  # TODO: We have no ambient light sensor until the tsl2561 sensor is working.  For now, output a space.
 c_temp = CpuTemp()
 temp_scale = conf_parser.get('main', 'temp_scale')
 if temp_scale.lower() not in ['f', 'fahrenheit', 'c', 'celsius']:
@@ -46,16 +45,16 @@ zone_names = ['zone_1', 'zone_2', 'zone_3', 'zone_4']
 garden_pi_zones = {}
 for zone_name in zone_names:
     alias = conf_parser.get(zone_name, 'alias')
-    moisture_sensor_gpio = conf_parser.getint(zone_name, 'moisture_sensor_gpio')
-    relay_gpio = conf_parser.getint(zone_name, 'relay_gpio')
+    moisture_sensor_pin = conf_parser.getint(zone_name, 'moisture_sensor_pin')
+    mcp_relay_pin = conf_parser.getint(zone_name, 'relay_gpio')
     moisture_water_threshold = conf_parser.getint(zone_name, 'moisture_water_threshold')
     watering_duration = conf_parser.getint(zone_name, 'watering_duration')
     temp_sensor_id = conf_parser.get(zone_name, 'temp_sensor_id')
     min_seconds_between_waterings = conf_parser.getint(zone_name, 'min_seconds_between_waterings')
     garden_pi_zones[zone_name] = Zone(name=zone_name,
                                       alias=alias,
-                                      moisture_sensor_pin=moisture_sensor_gpio,
-                                      mcp_pin=relay_gpio,
+                                      moisture_sensor_pin=moisture_sensor_pin,
+                                      mcp_pin=mcp_relay_pin,
                                       moisture_water_threshold=moisture_water_threshold,
                                       watering_duration=watering_duration,
                                       min_seconds_between_waterings=min_seconds_between_waterings,
@@ -105,7 +104,8 @@ class MeasurementData(object):
 
     @run_as_thread
     def get_ambient_light(self):
-        self._measurements['ambient_light'] = ambient_light_sensor.rc_count()
+        #self._measurements['ambient_light'] = ambient_light_sensor.rc_count()
+        self._measurements['ambient_light'] = 0  # TODO: We have no ambient light sensor until the tsl2561 sensor is working.  For now, output a 0.
 
     @run_as_thread
     def get_ambient_temp(self):
@@ -154,13 +154,44 @@ def log_measurements(measurement_d):
                              cpu_temp=measurement_d['cpu_temp'])
 
 
-def water_zone(zone_name):
-    # TODO: handle boolean return from garden_pi_zones[zone_name].water()
-    s = time.time()
-    garden_pi_logger.log_csv(messg='START WATERING: %s' % zone_name.upper())
-    garden_pi_zones[zone_name].water()
-    f = time.time()
-    garden_pi_logger.log_csv(messg='END WATERING: %s. Elapsed Time: %ss' % (zone_name.upper(), format_float(f-s)))
+def water_zone(zone_name, force_water=False):
+    """
+    Trigger watering of a zone by moisture sensor
+
+    :param zone_name: The name of the zone as used in the zone section header in the config file (i.e. 'zone_1', 'zone_4')
+    :type zone_name: str
+    """
+    water = False
+    if (garden_pi_zones[zone_name].seconds_before_ok_to_water == 0) and (garden_pi_zones[zone_name].moisture < garden_pi_zones[zone_name].moisture_water_threshold):
+        water = True
+    if force_water is True:
+        water = True
+    if water is True:
+        s = time.time()
+        garden_pi_logger.log_csv(messg='START WATERING (MOISTURE SENSOR): %s' % zone_name.upper())
+        garden_pi_zones[zone_name].water()
+        f = time.time()
+        garden_pi_logger.log_csv(messg='END WATERING (MOISTURE SENSOR): %s. Elapsed Time: %ss' % (zone_name.upper(), format_float(f-s)))
+
+
+def water_zone_now(zone_name, force_water=False):
+    """
+    Water a zone.  Mostly used byt a scheduled watering (not a sensor based watering)
+    :param zone_name: The name of the zone as used in the zone section header in the config file (i.e. 'zone_1', 'zone_4')
+    :type zone_name: str
+    :return:
+    """
+    water = False
+    if garden_pi_zones[zone_name].seconds_before_ok_to_water == 0:
+        water = True
+    if force_water is True:
+        water = True
+    if water is True:
+        s = time.time()
+        garden_pi_logger.log_csv(messg='START WATERING (SCHEDULED): %s' % zone_name.upper())
+        garden_pi_zones[zone_name].water()
+        f = time.time()
+        garden_pi_logger.log_csv(messg='END WATERING (SCHEDULED): %s. Elapsed Time: %ss' % (zone_name.upper(), format_float(f-s)))
 
 
 def cleanup(force_exit=False):
@@ -187,9 +218,11 @@ print('Starting Garden Pi')
 signal.signal(signal.SIGINT, signal_handler)
 scheduler = BackgroundScheduler()
 measurement_data = MeasurementData()
-scheduler.add_job(log_measurements, args=[measurement_data], trigger='cron', minute='*/5', name='Data Logger', id='data_logger', max_instances=1, misfire_grace_time=20)
+#scheduler.add_job(log_measurements, args=[measurement_data], trigger='cron', minute='*/5', name='Data Logger', id='data_logger', max_instances=1, misfire_grace_time=20)
+scheduler.add_job(log_measurements, args=[measurement_data], trigger='cron', minute='*', name='Data Logger', id='data_logger', max_instances=1, misfire_grace_time=20)
 for zone_name in garden_pi_zones.keys():
-    scheduler.add_job(water_zone, trigger='cron', hour='7,18', minute=15, args=[zone_name], name=zone_name, max_instances=1, misfire_grace_time=20)
+    #scheduler.add_job(water_zone, trigger='cron', hour='7,18', minute=15, args=[zone_name], name=zone_name, max_instances=1, misfire_grace_time=20)
+    scheduler.add_job(water_zone, trigger='cron', minute="*", args=[zone_name], name=zone_name, max_instances=1, misfire_grace_time=20)
 scheduler.start()
 for job in scheduler.get_jobs():
     print("Job: %s, Func: %s, Next run time: %s" % (job.name, job.func_ref, job.next_run_time))
